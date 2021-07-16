6689b13b6ba7b43969d486b348de35aee037df3a
Victor Hernandez <victoralfonso920@gmail.com>
2021-07-16 09:59:11 -0600

    donwgrade v alpha
    donwgrade v alpha

--- app/src/main/java/com/example/navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -3,36 +3,24 @@ package com.example.navigationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.navigationcompose.ui.theme.NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            SimpleComposable()
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SimpleComposable() {
+    Text("hola mundo")
 }
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    NavigationComposeTheme {
-        Greeting("Android")
-    }
+fun ComposablePreview() {
+    SimpleComposable()
 }
